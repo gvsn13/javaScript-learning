b1965aead43239ffeff18ec414e4b13311576698
gvsn13 <86527158+gvsn13@users.noreply.github.com>
2023-05-10 15:15:39 +0000

    video 5th

---
+++ 01_basics/02_data_Type.js
@@ -0,0 +1,28 @@
+//"use strict"; // treat all js code as newer version.
+
+//alert("hello") // we can not use alert in node js.
+
+let name = "Gaurav"
+let age = 23
+let male = true;
+let city;
+
+/*
+JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+
+in js null is a object dataType;
+
+*/
+
+//console.log(typeof name);
+console.log(typeof age);
+console.log(typeof male);
+console.log(typeof city);
