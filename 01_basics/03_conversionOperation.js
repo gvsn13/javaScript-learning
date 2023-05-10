1405a72afbcd1d0093281b26e3633f300f41f483
gvsn13 <86527158+gvsn13@users.noreply.github.com>
2023-05-10 21:57:39 +0530

    video 6th

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,7 @@
+let score = 33
+let score1 = String(score) // this is how we convert any data type to another data type.
+console.log(score)
+console.log(score1)
+console.log(typeof score)
+console.log(typeof score1)
+
