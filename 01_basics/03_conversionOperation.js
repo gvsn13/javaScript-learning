fda8377dd6be437f26880715f1c58c39ecbe0c8e
gvsn13 <86527158+gvsn13@users.noreply.github.com>
2023-05-13 00:46:16 +0530

    video 6th and 7th

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,3 +5,21 @@ console.log(score1)
 console.log(typeof score)
 console.log(typeof score1)
 
+//********************* Operations **************************
+// video 7.
+
+let val = 3
+let negValue = -val
+//console.log(negValue);
+
+console.log(2+2)
+console.log(2+"2")
+console.log("2"+2)
+console.log("2"+2+2)
+console.log(2+2+"2")
+
+/*
+ if first digit is string then the whole digit get converted into string. exmple in line no 17.
+ if last digit is string the the first all number perform operation and then get converted in string .exmple in line no 18.
+*/
+
